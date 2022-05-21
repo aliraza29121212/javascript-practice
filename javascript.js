c63c25b5f9dcf6d03cc957e322ed0eb9d596b75e
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-05-21 13:53:40 -0700

    add

---
+++ javascript.js
@@ -0,0 +1,53 @@
+// // // console.log("Hello World");
+
+// // // => 2
+// // "use strict";
+// // //intro to variables
+
+// // // Variable can store some information
+// // // We can use that information later
+// // // We can change that that information later
+
+// // // declare a variable
+
+// // // var firstName = "Ali Raza";
+
+// // // use a variable
+// // console.log(firstName);
+
+// // // change Value
+
+// // firstName = "Hamza";
+// // console.log(firstName);
+
+// //
+// //===============> (3)
+// //
+
+// // Rules for naming variables
+
+// // you cannot start with number
+// // example :-
+// // 1value (Invalid)
+// // value1 (Valid)
+
+// var value1 = 20;
+// console.log(value1 + 30);
+
+// // You can only use undersore _ or dollar symbol
+// // first_name (Valid)
+// // _firstname (Valid)
+
+// // first$name (Valid)
+// // $firstname (Valid)
+
+// // You cannot use spaces
+
+// // First name (Invalid)
+
+// // Convention
+// // Start with samll letter and use camelCase
+
+// //
+// //===============> (4)
+// //

df718959ee7d57a17d1edb1616cf582c15c2aba9
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-05-22 04:32:25 -0700

    add

--- javascript.js
+++ javascript.js
@@ -1,53 +1,157 @@
-// // // console.log("Hello World");
+// // // // // // // // // console.log("Hello World");
 
-// // // => 2
-// // "use strict";
-// // //intro to variables
+// // // // // // // // // => 2
+// // // // // // // // "use strict";
+// // // // // // // // //intro to variables
 
-// // // Variable can store some information
-// // // We can use that information later
-// // // We can change that that information later
+// // // // // // // // // Variable can store some information
+// // // // // // // // // We can use that information later
+// // // // // // // // // We can change that that information later
 
-// // // declare a variable
+// // // // // // // // // declare a variable
 
-// // // var firstName = "Ali Raza";
+// // // // // // // // // var firstName = "Ali Raza";
 
-// // // use a variable
-// // console.log(firstName);
+// // // // // // // // // use a variable
+// // // // // // // // console.log(firstName);
 
-// // // change Value
+// // // // // // // // // change Value
 
-// // firstName = "Hamza";
-// // console.log(firstName);
+// // // // // // // // firstName = "Hamza";
+// // // // // // // // console.log(firstName);
 
-// //
-// //===============> (3)
-// //
+// // // // // // // //
+// // // // // // // //===============> (3)
+// // // // // // // //
 
-// // Rules for naming variables
+// // // // // // // // Rules for naming variables
 
-// // you cannot start with number
-// // example :-
-// // 1value (Invalid)
-// // value1 (Valid)
+// // // // // // // // you cannot start with number
+// // // // // // // // example :-
+// // // // // // // // 1value (Invalid)
+// // // // // // // // value1 (Valid)
 
-// var value1 = 20;
-// console.log(value1 + 30);
+// // // // // // // var value1 = 20;
+// // // // // // // console.log(value1 + 30);
 
-// // You can only use undersore _ or dollar symbol
-// // first_name (Valid)
-// // _firstname (Valid)
+// // // // // // // // You can only use undersore _ or dollar symbol
+// // // // // // // // first_name (Valid)
+// // // // // // // // _firstname (Valid)
 
-// // first$name (Valid)
-// // $firstname (Valid)
+// // // // // // // // first$name (Valid)
+// // // // // // // // $firstname (Valid)
 
-// // You cannot use spaces
+// // // // // // // // You cannot use spaces
 
-// // First name (Invalid)
+// // // // // // // // First name (Invalid)
 
-// // Convention
-// // Start with samll letter and use camelCase
+// // // // // // // // Convention
+// // // // // // // // Start with samll letter and use camelCase
 
-// //
-// //===============> (4)
-// //
+// // // // // // // //
+// // // // // // // //===============> (4)
+// // // // // // // //
+
+// // // // // // // let Keyword
+// // // // // // // declare variable with let Keyword
+
+// // // // // // let firstName = "Ali raza";
+// // // // // // console.log(firstName);
+// // // // // // firstName = "Hammas";
+// // // // // // console.log(firstName);
+
+// // // // // // // block scope vs function scope (Coerd leter in this video)
+
+// // // // // // // //
+// // // // // // // //===============> (5)
+// // // // // // // //
+
+// // // // // // Declare Constants
+// // // // // // Const cannot change the value
+
+// // // // // const pi = 3.45;
+// // // // // console.log(pi);
+
+// // // // // // // //
+// // // // // // // //===============> (6)
+// // // // // // // //
+
+// // // // // Sting indexing
+
+// // // // let firstname = "ali";
+
+// // // // // a l i
+// // // // // 0 1 2
+
+// // // // // console.log(firstname[0]);
+
+// // // // // length of string
+// // // // // firstname.length
+
+// // // // // console.log(firstname.length);
+
+// // // // // last index : length -1
+// // // // console.log(firstname[firstname.length - 2]);
+
+// // // // // // // //
+// // // // // // // //===============> (7)
+// // // // // // // //
+
+// // // // trim()
+// // // // topUpperCase()
+// // // // topLowerCase()
+// // // // slice
+
+// // // let firstName = "aLi";
+// // // // console.log(firstName.length);
+// // // // firstName = firstName.trim();
+// // // // console.log(firstName.length);
+// // // // firstName = firstName.toUpperCase();
+// // // // firstName = firstName.toLowerCase();
+// // // // console.log(firstName);
+
+// // // // Start index
+// // // // end index
+
+// // // let newString = firstName.slice(0, 2);
+// // // console.log(newString);
+
+// // // // // // // //
+// // // // // // // //===============> (8)
+// // // // // // // //
+
+// // // type of operator
+
+// // // data types (primitive data types)
+// // // String
+// // // Number
+// // // booleans
+// // // undefined
+// // // null
+// // // bigInt
+// // // symbol
+
+// // let age = 22;
+// // let firstName = "Ali";
+// // // console.log(typeof firstName);
+
+// // // Convert Number to String
+// // age = age + "";
+// // console.log(typeof age);
+
+// // let age = 44;
+// // age = String(age);
+// // console.log(typeof age);
+
+// // // Convert string to Number
+
+// // myStrng = +"Ali";
+// // console.log(typeof myStrng);
+
+// let age = "44";
+// age = Number(age);
+// console.log(typeof age);
+
+// // // // // // // //
+// // // // // // // //===============> (9)
+// // // // // // // //

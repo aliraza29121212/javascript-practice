ea9733f60d04399da6b9815ab92d42c96fae43fd
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-05-22 11:01:03 -0700

    add

--- javascript.js
+++ javascript.js
@@ -1,157 +1,241 @@
-// // // // // // // // // console.log("Hello World");
+// // // // // // // // // // // // // console.log("Hello World");
 
-// // // // // // // // // => 2
-// // // // // // // // "use strict";
-// // // // // // // // //intro to variables
+// // // // // // // // // // // // // => 2
+// // // // // // // // // // // // "use strict";
+// // // // // // // // // // // // //intro to variables
 
-// // // // // // // // // Variable can store some information
-// // // // // // // // // We can use that information later
-// // // // // // // // // We can change that that information later
+// // // // // // // // // // // // // Variable can store some information
+// // // // // // // // // // // // // We can use that information later
+// // // // // // // // // // // // // We can change that that information later
 
-// // // // // // // // // declare a variable
+// // // // // // // // // // // // // declare a variable
 
-// // // // // // // // // var firstName = "Ali Raza";
+// // // // // // // // // // // // // var firstName = "Ali Raza";
 
-// // // // // // // // // use a variable
+// // // // // // // // // // // // // use a variable
+// // // // // // // // // // // // console.log(firstName);
+
+// // // // // // // // // // // // // change Value
+
+// // // // // // // // // // // // firstName = "Hamza";
+// // // // // // // // // // // // console.log(firstName);
+
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (3)
+// // // // // // // // // // // //
+
+// // // // // // // // // // // // Rules for naming variables
+
+// // // // // // // // // // // // you cannot start with number
+// // // // // // // // // // // // example :-
+// // // // // // // // // // // // 1value (Invalid)
+// // // // // // // // // // // // value1 (Valid)
+
+// // // // // // // // // // // var value1 = 20;
+// // // // // // // // // // // console.log(value1 + 30);
+
+// // // // // // // // // // // // You can only use undersore _ or dollar symbol
+// // // // // // // // // // // // first_name (Valid)
+// // // // // // // // // // // // _firstname (Valid)
+
+// // // // // // // // // // // // first$name (Valid)
+// // // // // // // // // // // // $firstname (Valid)
+
+// // // // // // // // // // // // You cannot use spaces
+
+// // // // // // // // // // // // First name (Invalid)
+
+// // // // // // // // // // // // Convention
+// // // // // // // // // // // // Start with samll letter and use camelCase
+
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (4)
+// // // // // // // // // // // //
+
+// // // // // // // // // // // let Keyword
+// // // // // // // // // // // declare variable with let Keyword
+
+// // // // // // // // // // let firstName = "Ali raza";
+// // // // // // // // // // console.log(firstName);
+// // // // // // // // // // firstName = "Hammas";
+// // // // // // // // // // console.log(firstName);
+
+// // // // // // // // // // // block scope vs function scope (Coerd leter in this video)
+
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (5)
+// // // // // // // // // // // //
+
+// // // // // // // // // // Declare Constants
+// // // // // // // // // // Const cannot change the value
+
+// // // // // // // // // const pi = 3.45;
+// // // // // // // // // console.log(pi);
+
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (6)
+// // // // // // // // // // // //
+
+// // // // // // // // // Sting indexing
+
+// // // // // // // // let firstname = "ali";
+
+// // // // // // // // // a l i
+// // // // // // // // // 0 1 2
+
+// // // // // // // // // console.log(firstname[0]);
+
+// // // // // // // // // length of string
+// // // // // // // // // firstname.length
+
+// // // // // // // // // console.log(firstname.length);
+
+// // // // // // // // // last index : length -1
+// // // // // // // // console.log(firstname[firstname.length - 2]);
+
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (7)
+// // // // // // // // // // // //
+
+// // // // // // // // trim()
+// // // // // // // // topUpperCase()
+// // // // // // // // topLowerCase()
+// // // // // // // // slice
+
+// // // // // // // let firstName = "aLi";
+// // // // // // // // console.log(firstName.length);
+// // // // // // // // firstName = firstName.trim();
+// // // // // // // // console.log(firstName.length);
+// // // // // // // // firstName = firstName.toUpperCase();
+// // // // // // // // firstName = firstName.toLowerCase();
 // // // // // // // // console.log(firstName);
 
-// // // // // // // // // change Value
+// // // // // // // // Start index
+// // // // // // // // end index
 
-// // // // // // // // firstName = "Hamza";
-// // // // // // // // console.log(firstName);
+// // // // // // // let newString = firstName.slice(0, 2);
+// // // // // // // console.log(newString);
 
-// // // // // // // //
-// // // // // // // //===============> (3)
-// // // // // // // //
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (8)
+// // // // // // // // // // // //
 
-// // // // // // // // Rules for naming variables
+// // // // // // // type of operator
 
-// // // // // // // // you cannot start with number
-// // // // // // // // example :-
-// // // // // // // // 1value (Invalid)
-// // // // // // // // value1 (Valid)
+// // // // // // // data types (primitive data types)
+// // // // // // // String
+// // // // // // // Number
+// // // // // // // booleans
+// // // // // // // undefined
+// // // // // // // null
+// // // // // // // bigInt
+// // // // // // // symbol
 
-// // // // // // // var value1 = 20;
-// // // // // // // console.log(value1 + 30);
+// // // // // // let age = 22;
+// // // // // // let firstName = "Ali";
+// // // // // // // console.log(typeof firstName);
 
-// // // // // // // // You can only use undersore _ or dollar symbol
-// // // // // // // // first_name (Valid)
-// // // // // // // // _firstname (Valid)
+// // // // // // // Convert Number to String
+// // // // // // age = age + "";
+// // // // // // console.log(typeof age);
 
-// // // // // // // // first$name (Valid)
-// // // // // // // // $firstname (Valid)
+// // // // // // let age = 44;
+// // // // // // age = String(age);
+// // // // // // console.log(typeof age);
 
-// // // // // // // // You cannot use spaces
+// // // // // // // Convert string to Number
 
-// // // // // // // // First name (Invalid)
+// // // // // // myStrng = +"Ali";
+// // // // // // console.log(typeof myStrng);
 
-// // // // // // // // Convention
-// // // // // // // // Start with samll letter and use camelCase
+// // // // // let age = "44";
+// // // // // age = Number(age);
+// // // // // console.log(typeof age);
 
-// // // // // // // //
-// // // // // // // //===============> (4)
-// // // // // // // //
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (9)
+// // // // // // // // // // // //
 
-// // // // // // // let Keyword
-// // // // // // // declare variable with let Keyword
+// // // // // String concatenation
 
-// // // // // // let firstName = "Ali raza";
-// // // // // // console.log(firstName);
-// // // // // // firstName = "Hammas";
-// // // // // // console.log(firstName);
+// // // // // let string1 = "Ali";
+// // // // // let string2 = "Raza";
 
-// // // // // // // block scope vs function scope (Coerd leter in this video)
+// // // // // let fullName = string1 + " " + string2;
+// // // // // console.log(fullName);
 
-// // // // // // // //
-// // // // // // // //===============> (5)
-// // // // // // // //
+// // // // // let string1 = "20";
+// // // // // let string2 = "40";
 
-// // // // // // Declare Constants
-// // // // // // Const cannot change the value
+// // // // // let newString = string1 + string2;
+// // // // // console.log(newString);
 
-// // // // // const pi = 3.45;
-// // // // // console.log(pi);
+// // // // // Convert string to Number
 
-// // // // // // // //
-// // // // // // // //===============> (6)
-// // // // // // // //
+// // // // let string1 = "20";
+// // // // let string2 = "40";
 
-// // // // // Sting indexing
+// // // // let newString = +string1 + +string2;
+// // // // console.log(newString);
+// // // // console.log(typeof newString);
 
-// // // // let firstname = "ali";
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (10)
+// // // // // // // // // // // //
 
-// // // // // a l i
-// // // // // 0 1 2
+// // // // Template Strings
 
-// // // // // console.log(firstname[0]);
+// // // let age = 20;
+// // // let firstName = "Ali";
 
-// // // // // length of string
-// // // // // firstname.length
+// // // // let aboutMe = "my name is " + firstName + " and my age is " + age;
 
-// // // // // console.log(firstname.length);
+// // // let aboutMe = `my name is ${firstName} and my age is ${age}`;
+// // // console.log(aboutMe);
 
-// // // // // last index : length -1
-// // // // console.log(firstname[firstname.length - 2]);
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (11)
+// // // // // // // // // // // //
 
-// // // // // // // //
-// // // // // // // //===============> (7)
-// // // // // // // //
+// // // Undefine
 
-// // // // trim()
-// // // // topUpperCase()
-// // // // topLowerCase()
-// // // // slice
+// // let firstName;
+// // console.log(typeof firstName);
+// // firstName = "Ali Raza";
+// // console.log(typeof firstName, firstName);
 
-// // // let firstName = "aLi";
-// // // // console.log(firstName.length);
-// // // // firstName = firstName.trim();
-// // // // console.log(firstName.length);
-// // // // firstName = firstName.toUpperCase();
-// // // // firstName = firstName.toLowerCase();
-// // // // console.log(firstName);
+// // // Null
 
-// // // // Start index
-// // // // end index
+// let myVariable = null;
+// console.log(myVariable);
+// myVariable = "Ali raza";
+// console.log(typeof myVariable, myVariable);
 
-// // // let newString = firstName.slice(0, 2);
-// // // console.log(newString);
+// console.log(typeof null); ==> javascript bug,error
 
-// // // // // // // //
-// // // // // // // //===============> (8)
-// // // // // // // //
+// BigIn
 
-// // // type of operator
+// let myNumber = BigInt(33);
+// let sameMyNumber = 127n;
+// console.log(myNumber + sameMyNumber);
+//console.log(typeof myNumber);
 
-// // // data types (primitive data types)
-// // // String
-// // // Number
-// // // booleans
-// // // undefined
-// // // null
-// // // bigInt
-// // // symbol
+// // // // // // // // // // // //
+// // // // // // // // // // // //===============> (11)
+// // // // // // // // // // // //
 
-// // let age = 22;
-// // let firstName = "Ali";
-// // // console.log(typeof firstName);
+// booleans & comparison operator
 
-// // // Convert Number to String
-// // age = age + "";
-// // console.log(typeof age);
+// booleans -> (true,false)
 
-// // let age = 44;
-// // age = String(age);
-// // console.log(typeof age);
+// let num1 = 4;
+// let num2 = "4";
+// console.log(num1 > num2);
 
-// // // Convert string to Number
+// == vs === Operator
 
-// // myStrng = +"Ali";
-// // console.log(typeof myStrng);
+// console.log(num1 === num2);
 
-// let age = "44";
-// age = Number(age);
-// console.log(typeof age);
+// != vs !== Operator
 
-// // // // // // // //
-// // // // // // // //===============> (9)
-// // // // // // // //
+// console.log(num1 !== num2);
